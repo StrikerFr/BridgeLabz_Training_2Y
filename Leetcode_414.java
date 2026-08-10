a72e5ed02ff0e2127e527e3520447c25202d40f6
Aryan <Aryann@gmail.com>
2026-08-10 09:32:53 +0530

    414

---
+++ Leetcode_414.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int thirdMax(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>(set);
+
+        Collections.sort(list, Collections.reverseOrder());
+
+        if (list.size() < 3) {
+            return list.get(0);
+        }
+
+        return list.get(2);
+    }
+}
