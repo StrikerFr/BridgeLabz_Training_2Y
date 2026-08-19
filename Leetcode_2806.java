66efed6793bbb8012e12ca11a1e7fa3e43472e93
Aryan <Aryann@gmail.com>
2026-08-19 14:30:03 +0530

    Mg

---
+++ Leetcode_2806.java
@@ -0,0 +1,6 @@
+class Leetcode_2806 {
+    public int accountBalanceAfterPurchase(int purchaseAmount) {
+        int rounded = ((purchaseAmount + 5) / 10) * 10;
+        return 100 - rounded;
+    }
+}
